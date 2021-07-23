935b812cace052f0e84051f12b37000799894d05
ciaran.corrigan <ciaran.corrigan@skipton.co.uk>
2021-07-23 11:17:42 +0100

    Add project files.

---
+++ RockPaperScissors/Controllers/RockPaperScissorsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Controllers
+{
+    public class RockPaperScissorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Leaderboard()
+        {
+            return View();
+        }
+    }
+}
