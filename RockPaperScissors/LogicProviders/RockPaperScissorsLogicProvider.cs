f72285be009ba4168492e5c724ab91c92e39491e
Ciaran Corrigan <ciaran.corrigan@skipton.co.uk>
2021-07-23 14:56:29 +0100

    Implement game logic template

---
+++ RockPaperScissors/LogicProviders/RockPaperScissorsLogicProvider.cs
@@ -0,0 +1,24 @@
+﻿using RockPaperScissors.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.LogicProviders
+{
+    public class RockPaperScissorsLogicProvider
+    {
+        //generate random selection
+        public Weapon GetRandomSelection()
+        {
+            return Weapon.Paper;
+        } 
+
+
+        //get result
+        public GameResult GetPlayerResult(Weapon player, Weapon computer)
+        {
+            return GameResult.Lose;
+        }
+    }
+}
